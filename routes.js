892c7af38fee244dad392e2a4311447da291b83d
Kheurt <59851560+Kheurt@users.noreply.github.com>
2022-12-29 23:02:31 +0100

    Update routes.js

--- routes.js
+++ routes.js
@@ -6,11 +6,6 @@ router.get('/', (req, res)=>{
     res.end('<h1 align="center">WELLCOME TO THE PROJECT BACKEND</h1>')
 })
 
-router.get('/', (req, res)=>{
-    debug.log("Access to / ")
-    res.end('<h1 align="center">WELLCOME TO THE PROJECT BACKEND</h1>')
-})
-
 router.post('/login', (req, res)=>{
     debug.log("Access to / ")
     res.end('<h1 align="center">WELLCOME TO THE PROJECT BACKEND</h1>')
