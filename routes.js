cc3cf67e410df08d8a38ebaf79ecb817a179c385
Kheurt <59851560+Kheurt@users.noreply.github.com>
2022-12-29 12:38:44 +0100

    Setup api routes

--- routes.js
+++ routes.js
@@ -6,4 +6,49 @@ router.get('/', (req, res)=>{
     res.end('<h1 align="center">WELLCOME TO THE PROJECT BACKEND</h1>')
 })
 
+router.get('/', (req, res)=>{
+    debug.log("Access to / ")
+    res.end('<h1 align="center">WELLCOME TO THE PROJECT BACKEND</h1>')
+})
+
+router.post('/login', (req, res)=>{
+    debug.log("Access to / ")
+    res.end('<h1 align="center">WELLCOME TO THE PROJECT BACKEND</h1>')
+})
+
+router.post('/signup', (req, res)=>{
+    debug.log("Access to / ")
+    res.end('<h1 align="center">WELLCOME TO THE PROJECT BACKEND</h1>')
+})
+
+router.post('/password-reset', (req, res)=>{
+    debug.log("Access to / ")
+    res.end('<h1 align="center">WELLCOME TO THE PROJECT BACKEND</h1>')
+})
+
+router.post('/new-contact', (req, res)=>{
+    debug.log("Access to / ")
+    res.end('<h1 align="center">WELLCOME TO THE PROJECT BACKEND</h1>')
+})
+
+router.post('/import', (req, res)=>{
+    debug.log("Access to / ")
+    res.end('<h1 align="center">WELLCOME TO THE PROJECT BACKEND</h1>')
+})
+
+router.get('/contacts', (req, res)=>{
+    debug.log("Access to / ")
+    res.end('<h1 align="center">WELLCOME TO THE PROJECT BACKEND</h1>')
+})
+
+router.post('/send', (req, res)=>{
+    debug.log("Access to / ")
+    res.end('<h1 align="center">WELLCOME TO THE PROJECT BACKEND</h1>')
+})
+
+router.get('/messages', (req, res)=>{
+    debug.log("Access to / ")
+    res.end('<h1 align="center">WELLCOME TO THE PROJECT BACKEND</h1>')
+})
+
 module.exports = router
